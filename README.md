ecb09788d7e9e4e4ea1c51b0495e12581114e029
Julio Cesar <jc.imp10@gmail.com>
2024-04-07 12:41:23 +0100

    conflits solve

--- README.md
+++ README.md
@@ -30,25 +30,21 @@ so that everything goes well:
 
 - Create an Init.SQL file with its DB creation structure
 
--~ Docker Build -T MySQL -Image.// To create the MySQL image configured on dockerfile
--~ Docker Run -d -v $ (pwd)/db/date:/var/lib/mysql - -name mysql -container mysql-image // to start a new container with the specified MySQL image, while alsoConfigures a bind volume to persist mysql database data on the host (standard port: 3306)
+- ~ Docker Build -T MySQL -Image.// To create the MySQL image configured on dockerfile
+- ~ Docker Run -d -v $ (pwd)/db/date:/var/lib/mysql - -name mysql -container mysql-image // to start a new container with the specified MySQL image, while alsoConfigures a bind volume to persist mysql database data on the host (standard port: 3306)
 
 - Now just create the connection with (MMT):
-    Open the mysql workbench.
-    Click on "New Connection" or "New Instance Connection".
-    Fill in the connection details:
-    Connection Name: A name for your connection (optional).
-    HostName: Use localhost if you are connecting from the same computer where the docker is being executed.If you are connecting from another computer, use the IP address of the computer where the docker is being executed.
-    Port: Use the mapped door you specified by starting the docker container, for example, 3306.
-    User Name: The MySQL username you set during the configuration.
-    Password: The MySQL password you set during the configuration.
-    Click "Test Connection" to check that the connection is working properly.
-    If everything is correct, click "OK" to save the connection and connect to the MySQL server.
+    - Open the mysql workbench.
+    - Click on "New Connection" or "New Instance Connection".
+    - Fill in the connection details:
+    - Connection Name: A name for your connection (optional).
+    - HostName: Use localhost if you are connecting from the same computer where the docker is being executed.If you are connecting from another computer, use the IP address of the computer where the docker is being executed.
+    - Port: Use the mapped door you specified by starting the docker container, for example, 3306.
+    - User Name: The MySQL username you set during the configuration.
+    - Password: The MySQL password you set during the configuration.
+    - Click "Test Connection" to check that the connection is working properly.
+    - If everything is correct, click "OK" to save the connection and connect to the MySQL server.
 
 # All ready
 - Take the opportunity to create your scripts with your darling
 - Click with the right button on the code and select Run MySQL Query and have fun
-
-
-
-
--- db/use cadastro;.sql
+++ db/use cadastro;.sql
@@ -1,65 +1,97 @@
 use cadastro;
 
--- show tables;
--- show columns from cursos;
--- show columns from gafanhotos;
+show tables;
+
+show columns
+FROM      cursos;
+
+show columns
+FROM      gafanhotos;
+
 -- Filtrar por colunas significa especificar  no select quais colunas vc quer mostrar na query
--- SELECT    *
--- FROM      gafanhotos;
--- SELECT    *
--- FROM      cursos;
---  Apenas com G , tornando não case sensitive e apenas valores únicos
--- SELECT    DISTINCT nome
--- FROM      gafanhotos
--- WHERE     BINARY LEFT (nome, 1) = 'G'; -- filtra nas linhas a partir de uma condição que seta a coluna nome
--- SELECT    DISTINCT nome
--- FROM      gafanhotos;
+SELECT    *
+FROM      gafanhotos;
+
+SELECT    *
+FROM      cursos;
+
+-- Apenas com G , tornando não case sensitive e apenas valores únicos
+SELECT    DISTINCT nome
+FROM      gafanhotos
+WHERE     BINARY LEFT (nome, 1) = 'G';
+
+-- filtra nas linhas a partir de uma condição que seta a coluna nome
+SELECT    DISTINCT nome
+FROM      gafanhotos;
+
 -- Apenas com G , tornando não case sensitive 
--- SELECT    *
--- FROM      gafanhotos
--- WHERE     BINARY LEFT (nome, 1) = 'G';
+SELECT    *
+FROM      gafanhotos
+WHERE     BINARY LEFT (nome, 1) = 'G';
+
 -- Ordenar usando o ORDER BY
--- SELECT    *
--- FROM      gafanhotos
--- ORDER BY  nome DESC;
--- SELECT    * 
--- FROM      cursos
--- ORDER BY  nome DESC;
--- SELECT    ano,nome,carga
--- FROM      cursos
--- ORDER BY  ano DESC,nome DESC,carga;
+SELECT    *
+FROM      gafanhotos
+ORDER BY  nome DESC;
+
+SELECT    *
+FROM      cursos
+ORDER BY  nome DESC;
+
+SELECT    ano,
+          nome,
+          carga
+FROM      cursos
+ORDER BY  ano DESC,
+          nome DESC,
+          carga;
+
 -- Usando condicionais com WHERE
--- SELECT nome, ano
--- FROM   cursos
--- WHERE ano IN (2015,2018) -- igual a 2015 ou igual a 2018
--- ORDER BY ano DESC;
--- SELECT nome, ano
--- FROM   cursos
--- WHERE ano BETWEEN 2015 AND 2018 -- sempre inclusive ambas as pontas 
--- ORDER BY ano DESC;
--- SELECT nome -- apenas coluna nome
--- FROM   cursos
--- WHERE ano BETWEEN 2015 AND 2018 -- sempre inclusive ambas as pontas 
--- ORDER BY ano DESC;
--- SELECT nome ,ano
--- FROM   cursos
--- WHERE ano NOT BETWEEN 2015 AND 2018 -- significa que não quero o intervalo especificado
--- ORDER BY ano DESC;
--- SELECT nome ,ano
--- FROM   cursos
--- WHERE ano > 2015 AND ano < 2018 -- retirando o 2015 e 2018 da consulta
--- ORDER BY ano DESC;
--- SELECT * 
--- FROM cursos
--- WHERE nome = 'PHP';
--- SELECT * 
--- FROM cursos
--- WHERE nome LIKE 'P%';
--- Funções de agregação (agrupamento)
--- SELECT * 
--- FROM cursos;
--- SELECT COUNT(*) -- contar todos inclusive os null
--- FROM cursos;
+SELECT    nome,
+          ano
+FROM      cursos
+WHERE     ano IN (2015, 2018) -- igual a 2015 ou igual a 2018
+ORDER BY  ano DESC;
+
+SELECT    nome,
+          ano
+FROM      cursos
+WHERE     ano BETWEEN 2015 AND 2018 -- sempre inclusive ambas as pontas 
+ORDER BY  ano DESC;
+
+SELECT    nome -- apenas coluna nome
+FROM      cursos
+WHERE     ano BETWEEN 2015 AND 2018 -- sempre inclusive ambas as pontas 
+ORDER BY  ano DESC;
+
+SELECT    nome,
+          ano
+FROM      cursos
+WHERE     ano NOT BETWEEN 2015 AND 2018 -- significa que não quero o intervalo especificado
+ORDER BY  ano DESC;
+
+SELECT    nome,
+          ano
+FROM      cursos
+WHERE     ano > 2015
+AND       ano < 2018 -- retirando o 2015 e 2018 da consulta
+ORDER BY  ano DESC;
+
+SELECT    *
+FROM      cursos
+WHERE     nome = 'PHP';
+
+SELECT    *
+FROM      cursos
+WHERE     nome LIKE 'P%';
+
+Funções de agregação (agrupamento)
+SELECT    *
+FROM      cursos;
+
+SELECT    COUNT(*) -- contar todos inclusive os null
+FROM      cursos;
+
 SELECT    (
           SELECT    COUNT(*)
           FROM      cursos
